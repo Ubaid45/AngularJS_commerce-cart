d5984c0d7205bfa2a4cc9d834089d765c032d994
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-21 23:33:08 +0200

    Initial commit with Firebase connection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBJqVWMS3_Vq0iwZdKUXh1k4GFGQhMlR1E",
+    authDomain: "oshop-d41ac.firebaseapp.com",
+    databaseURL: "https://oshop-d41ac.firebaseio.com",
+    projectId: "oshop-d41ac",
+    storageBucket: "oshop-d41ac.appspot.com",
+    messagingSenderId: "168857308590",
+    appId: "1:168857308590:web:0439d1f786dc7ae6f34c3f"
+  }
 };
 
 /*
