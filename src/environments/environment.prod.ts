de333de48acee26817bd71ebe63c1e731ce611aa
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-22 02:19:53 +0200

    deploy the application to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBJqVWMS3_Vq0iwZdKUXh1k4GFGQhMlR1E",
+    authDomain: "oshop-d41ac.firebaseapp.com",
+    databaseURL: "https://oshop-d41ac.firebaseio.com",
+    projectId: "oshop-d41ac",
+    storageBucket: "oshop-d41ac.appspot.com",
+    messagingSenderId: "168857308590",
+    appId: "1:168857308590:web:0439d1f786dc7ae6f34c3f"
+  }
 };
