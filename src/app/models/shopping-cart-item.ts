5add9d6c58a9064766dbf982fcb7293eead4f896
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-24 12:10:29 +0200

    Display quantity on cart page

--- src/app/models/shopping-cart-item.ts
+++ src/app/models/shopping-cart-item.ts
@@ -4,12 +4,15 @@ export class ShoppingCartItem {
   $key: string;
   title: string;
   imageUrl: string;
-  price: number; 
-  quantity: number; 
+  price: number;
+  //quantity: number;
 
-  constructor(init?: Partial<ShoppingCartItem>) {
+  constructor(public product: Product, public quantity: number) {
+    //Object.assign(this, init);
+  }
+  /*constructor(init?: Partial<ShoppingCartItem>) {
     Object.assign(this, init);
   }
-
-  get totalPrice() { return this.price * this.quantity; }
+*/
+  get totalPrice() { return this.product.price * this.quantity; }
 }
