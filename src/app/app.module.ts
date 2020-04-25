03ed48dc9d875bbcd47513bf03145b3d15c21cce
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-25 12:42:24 +0200

    Created a shared module and imported references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,35 @@
-import { OrderService } from 'shared/services/order.service';
-import { ShoppingCartService } from 'shared/services/shopping-cart.service';
-import { ProductService } from 'shared/services/product.service';
-import { CategoryService } from 'shared/services/category.service';
-import { AdminAuthGuard } from 'shared/services/admin-auth-guard.service';
-import { UserService } from 'shared/services/user.service';
-import { AuthService } from 'shared/services/auth.service';
-import { LoginComponent } from './login/login.component';
-import { environment } from './../environments/environment';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { CustomFormsModule } from 'ng2-validation';
-//import { DataTableModule } from 'angular-4-data-table';
+import { AuthGuard } from 'shared/services/auth-guard.service';
 
+import { environment } from './../environments/environment';
+import { AdminAuthGuard } from './admin-auth-guard.service';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { ProductFormComponent } from './admin/product-form/product-form.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckOutComponent } from './check-out/check-out.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
-import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-import { AuthGuard } from 'shared/services/auth-guard.service';
-import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
-import { ProductCardComponent } from './product-card/product-card.component';
-import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
+import { ProductsComponent } from './products/products.component';
+import { SharedModule } from './shared/shared.module';
 import { ShippingFormComponent } from './shipping-form/shipping-form.component';
 import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+
+//import { DataTableModule } from 'angular-4-data-table';
 
 @NgModule({
   declarations: [
@@ -51,13 +45,12 @@ import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-c
     AdminOrdersComponent,
     ProductFormComponent,
     ProductFilterComponent,
-    ProductCardComponent,
-    ProductQuantityComponent,
     ShippingFormComponent,
     ShoppingCartSummaryComponent,
   ],
   imports: [
     BrowserModule,
+    SharedModule,
     FormsModule,
     CustomFormsModule,
     //DataTableModule,
@@ -83,14 +76,7 @@ import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-c
     ])
   ],
   providers: [
-    AuthService,
-    AuthGuard,
-    AdminAuthGuard,
-    UserService,
-    CategoryService,
-    ProductService,
-    ShoppingCartService,
-    OrderService
+    AdminAuthGuard
   ],
   bootstrap: [AppComponent]
 })
